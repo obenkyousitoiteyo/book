847656f008e3be14412e04f915b00996149898b3
mukai kyousuke <c0404@oic.jp>
2022-12-06 15:57:40 +0900

    Uemura

--- bookproject/book/urls.py
+++ bookproject/book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns=[
-    path('book/',views.ListBookView.as_view()),
+    path('book/',views.ListBookView.as_view(),name='list-book'),
     path('book/<int:pk>/detail/',views.DetailBookView.as_view()),
+    path('book/toukou/',views.Toukou.as_view(), name='create'),
 ]
--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 from.models import Book
+from django.urls import reverse,reverse_lazy
 
 class ListBookView(ListView):
     template_name='book/Diary.html'
     model=Book 
+    # object_list = Book.objects.order_by('id')
+    # return render(request, 'book/Diary.html', {'object_list':object_list})
 
 class DetailBookView(DetailView):
     template_name='book/book_detail.html'
     model=Book
-# Create your views here.
+
+class Toukou(CreateView):
+    template_name = 'book/toukou.html'
+    model = Book
+    fields = ('title','text')
+    success_url = reverse_lazy('list-book')
