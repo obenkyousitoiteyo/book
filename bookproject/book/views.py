ca87dd8225736c1db1a746ec6236ef0a7b344613
mukai kyousuke <c0404@oic.jp>
2022-12-09 14:38:22 +0900

    honmono

--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import (ListView,DetailView,CreateView,DeleteView,UpdateView)
 from.models import Book
 from django.urls import reverse,reverse_lazy
 
+
 class ListBookView(ListView):
+    object_list = Book.objects.order_by('id').reverse()
     template_name='book/Diary.html'
     model=Book 
+    
     # object_list = Book.objects.order_by('id')
     # return render(request, 'book/Diary.html', {'object_list':object_list})
+
 
 class DetailBookView(DetailView):
     template_name='book/book_detail.html'
@@ -18,3 +22,16 @@ class Toukou(CreateView):
     model = Book
     fields = ('title','text')
     success_url = reverse_lazy('list-book')
+
+class Delete(DeleteView):
+    template_name = 'book/book_confirm_delete.html'
+    model = Book
+    success_url = reverse_lazy('list-book')
+
+
+class Update(UpdateView):
+    template_name = 'book/book_update.html'
+    model = Book
+    fields = ('title', 'text')
+    success_url = reverse_lazy('list-book')
+    
